8b60f2748528214bc1b5bd439e38776d66ae1f23
gideon-oteng <devopsgideon@gmail.com>
2021-07-31 16:23:59 -0400

    assignment

---
+++ Controllers/GONameAddressesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GOClubs.Models;
+
+namespace GOClubs.Controllers
+{
+    public class GONameAddressesController : Controller
+    {
+        private readonly GOClubsContext _context;
+
+        public GONameAddressesController(GOClubsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: GONameAddresses
+        public async Task<IActionResult> Index()
+        {
+            var gOClubsContext = _context.NameAddress.Include(n => n.ProvinceCodeNavigation);
+            return View(await gOClubsContext.ToListAsync());
+        }
+
+        // GET: GONameAddresses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nameAddress = await _context.NameAddress
+                .Include(n => n.ProvinceCodeNavigation)
+                .FirstOrDefaultAsync(m => m.NameAddressId == id);
+            if (nameAddress == null)
+            {
+                return NotFound();
+            }
+
+            return View(nameAddress);
+        }
+
+        // GET: GONameAddresses/Create
+        public IActionResult Create()
+        {
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode");
+            return View();
+        }
+
+        // POST: GONameAddresses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NameAddressId,FirstName,LastName,CompanyName,StreetAddress,City,PostalCode,ProvinceCode,Email,Phone")] NameAddress nameAddress)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(nameAddress);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", nameAddress.ProvinceCode);
+            return View(nameAddress);
+        }
+
+        // GET: GONameAddresses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nameAddress = await _context.NameAddress.FindAsync(id);
+            if (nameAddress == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", nameAddress.ProvinceCode);
+            return View(nameAddress);
+        }
+
+        // POST: GONameAddresses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("NameAddressId,FirstName,LastName,CompanyName,StreetAddress,City,PostalCode,ProvinceCode,Email,Phone")] NameAddress nameAddress)
+        {
+            if (id != nameAddress.NameAddressId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(nameAddress);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NameAddressExists(nameAddress.NameAddressId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProvinceCode"] = new SelectList(_context.Province, "ProvinceCode", "ProvinceCode", nameAddress.ProvinceCode);
+            return View(nameAddress);
+        }
+
+        // GET: GONameAddresses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nameAddress = await _context.NameAddress
+                .Include(n => n.ProvinceCodeNavigation)
+                .FirstOrDefaultAsync(m => m.NameAddressId == id);
+            if (nameAddress == null)
+            {
+                return NotFound();
+            }
+
+            return View(nameAddress);
+        }
+
+        // POST: GONameAddresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var nameAddress = await _context.NameAddress.FindAsync(id);
+            _context.NameAddress.Remove(nameAddress);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NameAddressExists(int id)
+        {
+            return _context.NameAddress.Any(e => e.NameAddressId == id);
+        }
+    }
+}
